dc93c1951a74097eb64e0069dd44a0c275f625e2
unnamed <mckcsgoyt@gmail.com>
2024-10-15 09:24:01 +0200

    Revert "first commit"
    This reverts commit 222680d5e352685bf9cfcbfd6e9c89a0f8c5ee4d.

--- src/utils/errors.js
+++
@@ -1,9 +0,0 @@
-export const handleError = (err, defaultErrorMessage) => {
-  let errorMessage;
-  if (err.response && err.response.data) {
-    errorMessage = err.response.data.message || defaultErrorMessage;
-  } else {
-    errorMessage = defaultErrorMessage;
-  }
-  return new Error(errorMessage);
-};
