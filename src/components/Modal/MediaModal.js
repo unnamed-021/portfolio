c17b8568a54cc246a9f7fc50e32b5377c47d2ad2
unnamed-021 <mckcsgoyt@gmail.com>
2024-10-21 14:25:49 +0200

    Update MediaModal.js

--- src/components/Modal/MediaModal.js
+++ src/components/Modal/MediaModal.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import { CloseSvg, StyledIconContainer, Image } from "./ModalComponent.styles";
 import Modal from "./ModalMedia";
